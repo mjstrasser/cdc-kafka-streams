1894dfd7da6e99fe51c33d8f3f56a0b77263ae7e
Michael Strasser <michael@michaelstrasser.com>
2023-03-07 12:38:37 +1000

    Improved tests; using Kotest HTML Reporter.

--- src/test/kotlin/mjs/cdc/AddressAndCustomerTest.kt
+++ src/test/kotlin/mjs/cdc/AddressAndCustomerTest.kt
@@ -1,5 +1,6 @@
 package mjs.cdc
 
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.collections.shouldHaveSize
 import mjs.cdc.helper.TopologyTestSpec
 import mjs.cdc.helper.database.addressData
@@ -10,11 +11,18 @@ import mjs.cdc.helper.database.headers
 import mjs.cdc.helper.randomId
 import mjs.cdc.helper.randomTxnId
 import mjs.database.header.operation
+import mjs.kotest.description
 
 class AddressAndCustomerTest : TopologyTestSpec({
 
-    describe("Inserting address and customer address records") {
-        it("produces one event for address and for customer") {
+    description(
+        """
+        Tests of inserts into Address and CustomerAddress tables in the same transaction. 
+    """.trimIndent()
+    )
+
+    describe("Inserting Address and CustomerAddress records") {
+        it("produces one `AddressCreatedEvent` and one `CustomerModifiedEvent`") {
             val txnId = randomTxnId()
             val customerId = randomId()
             val addressId = randomId()
@@ -32,6 +40,12 @@ class AddressAndCustomerTest : TopologyTestSpec({
             val events = readOutputs()
 
             events shouldHaveSize 2
+            events.map { it.value::class.java.simpleName }.toSet().shouldContainExactly(
+                setOf(
+                    "AddressCreatedEvent",
+                    "CustomerModifiedEvent",
+                )
+            )
         }
     }
 
--- src/test/kotlin/mjs/cdc/AddressCreatedTest.kt
+++ src/test/kotlin/mjs/cdc/AddressCreatedTest.kt
@@ -1,16 +1,26 @@
 package mjs.cdc
 
+import io.kotest.inspectors.shouldForAll
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.types.shouldBeTypeOf
 import mjs.cdc.helper.TopologyTestSpec
 import mjs.cdc.helper.database.addressMessage
 import mjs.cdc.helper.database.headers
 import mjs.cdc.helper.randomTxnId
 import mjs.database.header.operation
+import mjs.entities.AddressCreatedEvent
+import mjs.kotest.description
 
 class AddressCreatedTest : TopologyTestSpec({
 
-    describe("Inserting address records into database") {
-        it("produces one record out from a single-record transaction") {
+    description(
+        """
+        Tests of inserting one or more Address records in the the database in a single transaction.
+    """.trimIndent()
+    )
+
+    describe("Inserting address records into the database") {
+        it("produces one `AddressCreatedEvent` from a single-record transaction") {
             val txnId = randomTxnId()
             sendInput(
                 addressMessage(headers(txnId, operation.INSERT, eventCounter = 1, lastEvent = true)),
@@ -19,8 +29,9 @@ class AddressCreatedTest : TopologyTestSpec({
             val events = readOutputs()
 
             events shouldHaveSize 1
+            events.first().value.shouldBeTypeOf<AddressCreatedEvent>()
         }
-        it("produces five records from a five-record transaction") {
+        it("produces five `AddressCreatedEvent`s from a five-record transaction") {
             val txnId = randomTxnId()
             sendInput(
                 addressMessage(headers(txnId, operation.INSERT, eventCounter = 1, lastEvent = false)),
@@ -33,7 +44,7 @@ class AddressCreatedTest : TopologyTestSpec({
             val events = readOutputs()
 
             events shouldHaveSize 5
+            events.shouldForAll { it.value.shouldBeTypeOf<AddressCreatedEvent>() }
         }
     }
-
 })
